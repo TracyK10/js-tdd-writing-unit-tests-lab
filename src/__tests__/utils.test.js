809a45a96068e92aafb27f30f7b6758e90bbf8e6
Tracy Karanja <chirutracy@gmail.com>
2024-05-01 22:44:18 +0300

    new commit

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,34 @@
 // Your tests here
+import { isPalidrome } from "../utils";
+
+describe("isPalindrome", () => {
+    it("return true is word is a Palindrome", () => {
+        const word = "racecar";
+        const result = isPalidrome(word);
+        expect(isPalidrome("racecar")).toBe(true);
+    })
+
+    it("returns false is word is not a Palindrome", () => {
+        const word = "car"
+        const result = isPalidrome(word);
+        expect(isPalidrome(word)).toBe(false);
+    })
+
+    it("returns true is word is a mixture of uppercase and lowercase letters", () => {
+        const word = "MoM"
+        const result = isPalidrome(word);
+        expect(isPalidrome(word)).toBe(true);
+    })
+
+    it("returns an error if word is an empty string", () => {
+        const word = ""
+        
+        expect(isPalidrome(word)).toBe(false);
+    })
+
+    it("throws an error is word is a non-alphabetic character", () => {
+        const word = "123"
+        
+        expect(() => isPalidrome(word)).toThrow("Word must be a string");
+    })
+})
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,13 @@
 // Your code here
+export function isPalidrome(word) {
+    const initialWord = word.split("").join("");
+    const result = word.split("").reverse().join("");
+    if (!/^[A-Za-z]+$/.test(word)) {
+        throw new Error("Word must be a string");
+    }
+    else if (initialWord === result) {
+        return true;
+    } else {
+        return false;
+    }
+}
